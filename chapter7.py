fff8f09de483b4d8dd9c1c45b2a4d8dfa88e44e3
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2022-06-22 20:33:14 +0100

    Finished Chapter 7

--- chapter7.py
+++ chapter7.py
@@ -117,3 +117,82 @@ while toppings != 'quit':
     toppings = input(prompt)
     if toppings != 'quit':
         print(f"{toppings} added")
+
+prompt = "Enter your age "
+prompt += "(Enter 'quit' to end the program)"
+age = ""
+while age != 'quit':
+    if age != 'quit':
+        age = input(prompt)
+        age = int(age)
+        if age < 3:
+            print("Ticket is free")
+        elif age < 12:
+            print("ticket is $10")
+        elif age > 12:
+            print("ticket is $15")
+    else:
+        break
+
+# using while loop with lists and dictionaries
+# moving items between lists
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+    print(f"Veifying user:{current_user.title()}")
+    confirmed_users.append(current_user)
+print("The following users are verified: ")
+for confirmed_user in confirmed_users:
+    print(confirmed_user.title())
+
+# removing all instances of value from a list
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+while 'cat' in pets:
+    pets.remove('cat')
+print(pets)
+
+# filling a dictionnary with a user input
+responses = {}
+polling_active = True
+while polling_active:
+    name = input(f"\nWhat is your name? ")
+    response = input("Which mountain would you lik to climb? ")
+    responses[name] = response
+    repeat = input('Would you like another person to response? (yes/no)')
+    if repeat == 'no':
+        polling_active = False
+print(f"\n---Polling Results---")
+for name, response in responses.items():
+    print(f"{name} would like to climb {response}")
+
+# ex 7-8
+sandwich_orders = ['tuna', 'egg', 'chicken', 'salad', 'prawns']
+finished_sandwiches = []
+while sandwich_orders:
+    preparing_sandwich = sandwich_orders.pop()
+    print(f"Preparing: {preparing_sandwich} sandwich")
+    finished_sandwiches.append(preparing_sandwich)
+for finished_sandwich in finished_sandwiches:
+    print(f"Ready to serve: {finished_sandwich} sandwich")
+
+sandwich_orders = ['tuna', 'pastrami', 'pastrami',
+                   'egg', 'chicken', 'pastrami', 'salad', 'prawns']
+print(sandwich_orders)
+print('Deli has run out of pastrami')
+while 'pastrami' in sandwich_orders:
+    sandwich_orders.remove('pastrami')
+print(sandwich_orders)
+
+dream_vacation = {}
+polling_active = True
+while polling_active:
+    name = input("Name?")
+    location = input('location?')
+    dream_vacation[name] = location
+    repeat = input('Would you like another person to response? (yes/no)')
+    if repeat == 'no':
+        polling_active = False
+for name, location in dream_vacation.items():
+    print(f"{name.title()}'s favourite location is {location.title()}")
