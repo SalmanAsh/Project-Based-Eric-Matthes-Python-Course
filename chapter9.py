8be49ebe2475cb99b036dec5121a5ca081676fea
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2022-06-26 19:02:24 +0100

    chapter 9

--- chapter9.py
+++ chapter9.py
@@ -160,3 +160,56 @@ print(my_new_car.get_descriptive_name())
 
 
 # setting a default value for an attribute
+class Car:
+    """A simple attempt to represent a car"""
+
+    def __init__(self, make, model, year):
+        """initialize attributes"""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometre_reading = 0
+
+    def get_descriptive_name(self):
+        """return a neatlt formatted descriptive name"""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometre(self):
+        """print a statement showing car's mileage"""
+        print(f"The car has {self.odometre_reading} miles on it")
+
+
+my_new_car = Car(make='open', model='corsa', year=2003)
+print(my_new_car.get_descriptive_name())
+my_new_car.read_odometre()
+
+
+# modifying an attibute value directly
+
+class Car:
+    """A simple attempt to represent a car"""
+
+    def __init__(self, make, model, year):
+        """initialize attributes"""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometre_reading = 0
+
+    def get_descriptive_name(self):
+        """return a neatlt formatted descriptive name"""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometre(self):
+        """print a statement showing car's mileage"""
+        print(f"The car has {self.odometre_reading} miles on it")
+
+
+my_new_car = Car(make='open', model='corsa', year=2003)
+print(my_new_car.get_descriptive_name())
+my_new_car.odometre_reading = 23
+my_new_car.read_odometre()
+
+# modifying an attibute value through a method
