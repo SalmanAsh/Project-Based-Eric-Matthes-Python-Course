bd3b0040d40666c7c2382622ab0a36b81be2e6ab
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2022-06-27 16:22:46 +0100

    chapter 9 exersize 9-6

--- chapter9.py
+++ chapter9.py
@@ -555,3 +555,97 @@ my_tesla = ElectricCar('tesla', 'model s', year=2019)
 print(my_tesla.get_descriptive_name())
 my_tesla.battery.describe_battery()
 my_tesla.battery.get_range()
+
+# ex 9-6
+
+
+class Restaurant:
+    """Restaurant info"""
+
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(
+            f"the restaurant name is {restaurant_name} and it offers {cuisine_type} cuisines")
+
+    def open_restaurant(self):
+        print("open")
+
+
+class IceCreamStand(Restaurant):
+    """initialize attributes"""
+
+    def __init__(self, restaurant_name, cuisine_type):
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavour = flavour
+
+    def icecream_flavours(self):
+        """Add flavours"""
+        print(f"adding {self.flavour}")
+
+
+class Car:
+    """A simple attempt to represent a car"""
+
+    def __init__(self, make, model, year):
+        """initialize attributes"""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometre_reading = 0
+
+    def get_descriptive_name(self):
+        """return a neatlt formatted descriptive name"""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometre(self):
+        """print a statement showing car's mileage"""
+        print(f"The car has {self.odometre_reading} miles on it")
+
+    def update_odometre(self, mileage):
+        """Update the odometer"""
+        """Reject any request to roll back the odometer"""
+        if mileage >= self.odometre_reading:
+            self.odometre_reading = mileage
+        else:
+            print("Error")
+
+
+class Battery:
+    """Electric car's battery"""
+
+    def __init__(self, battery_size=85):
+        """Initialize attributes"""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """battery size"""
+        print(f"This car has a {self.battery_size}-kWh battery")
+
+    def get_range(self):
+        """Print a statement about the range"""
+        if self.battery_size == 75:
+            range = 260
+            print(f"This car can go about {range} miles on full charge")
+        elif self.battery_size == 80:
+            range = 315
+            print(f"This car can go about {range} miles on full charge")
+
+    def update_battery(self):
+        """Capacity"""
+        Capacity = 100
+
+
+class ElectricCar(Car):
+    """Electric car feature"""
+
+    def __init__(self, make, model, year):
+        """initialize Attributes"""
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+
+# Importing a single class
