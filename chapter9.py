35f78c6ed7d7a30dee67cb52b3a072e9bb881631
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2022-06-27 15:43:26 +0100

    Instances as attributes chapter 9

--- chapter9.py
+++ chapter9.py
@@ -408,3 +408,150 @@ class ElectricCar(Car):
 my_tesla = ElectricCar('tesla', 'model s', year=2019)
 print(my_tesla.get_descriptive_name())
 my_tesla.describe_battery()
+
+
+# overriding a method in child class
+class Car:
+    """A simple attempt to represent a car"""
+
+    def __init__(self, make, model, year):
+        """initialize attributes"""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometre_reading = 0
+
+    def get_descriptive_name(self):
+        """return a neatlt formatted descriptive name"""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+        def fill_gas_tank(self):
+            print("the car has a gas tank")
+
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+
+    def fill_gas_tank():
+        print("electric car do not have a gas tank")
+
+# instances as attributes
+
+
+class Car:
+    """A simple attempt to represent a car"""
+
+    def __init__(self, make, model, year):
+        """initialize attributes"""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometre_reading = 0
+
+    def get_descriptive_name(self):
+        """return a neatlt formatted descriptive name"""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometre(self):
+        """print a statement showing car's mileage"""
+        print(f"The car has {self.odometre_reading} miles on it")
+
+    def update_odometre(self, mileage):
+        """Update the odometer"""
+        """Reject any request to roll back the odometer"""
+        if mileage >= self.odometre_reading:
+            self.odometre_reading = mileage
+        else:
+            print("Error")
+
+
+class Battery:
+    """Electric car's battery"""
+
+    def __init__(self, battery_size=85):
+        """Initialize attributes"""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """battery size"""
+        print(f"This car has a {self.battery_size}-kWh battery")
+
+
+class ElectricCar(Car):
+    """Electric car feature"""
+
+    def __init__(self, make, model, year):
+        """initialize Attributes"""
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+
+my_tesla = ElectricCar('tesla', 'model s', year=2019)
+my_tesla.battery.describe_battery()
+
+
+class Car:
+    """A simple attempt to represent a car"""
+
+    def __init__(self, make, model, year):
+        """initialize attributes"""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometre_reading = 0
+
+    def get_descriptive_name(self):
+        """return a neatlt formatted descriptive name"""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometre(self):
+        """print a statement showing car's mileage"""
+        print(f"The car has {self.odometre_reading} miles on it")
+
+    def update_odometre(self, mileage):
+        """Update the odometer"""
+        """Reject any request to roll back the odometer"""
+        if mileage >= self.odometre_reading:
+            self.odometre_reading = mileage
+        else:
+            print("Error")
+
+
+class Battery:
+    """Electric car's battery"""
+
+    def __init__(self, battery_size=85):
+        """Initialize attributes"""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """battery size"""
+        print(f"This car has a {self.battery_size}-kWh battery")
+
+    def get_range(self):
+        """Print a statement about the range"""
+        if self.battery_size == 75:
+            range = 260
+            print(f"This car can go about {range} miles on full charge")
+        elif self.battery_size == 80:
+            range = 315
+            print(f"This car can go about {range} miles on full charge")
+
+
+class ElectricCar(Car):
+    """Electric car feature"""
+
+    def __init__(self, make, model, year):
+        """initialize Attributes"""
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+
+my_tesla = ElectricCar('tesla', 'model s', year=2019)
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
